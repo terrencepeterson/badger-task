37057628b9f0d8002255e63e82e24656a1103820
terrencepeterson <mattbtindall@gmail.com>
2025-03-13 09:18:46 +0000

    Add nodejs alpine container with simple test api files

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,7 +1,11 @@
-require("dotenv").config()
-const express = require('express')
+//require("dotenv").config()
+import '@dotenvx/dotenvx/config'
+import express from "express"
+import cors from "cors"
+
 const app = express()
 app.use(express.json())
+app.use(cors())
 const { server_host: host, server_port: port } = process.env
 
 app.get('/', (req, res) => {
@@ -12,7 +16,13 @@ app.get('/', (req, res) => {
     ])
 })
 
+app.post('/sign-up', (req, res) => {
+    console.log('test post')
+    console.log(req.body)
+    res.send(req.body)
+})
+
 app.listen(port, host, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port`)
 })
 
