3bbda94de35c1b220f43f1fdc82b5ce724dd097e
terrencepeterson <mattbtindall@gmail.com>
2025-05-20 11:29:29 +0100

    Add admin role access control middleware

--- backend/api/roleAccess.mjs
+++ backend/api/roleAccess.mjs
@@ -1,11 +1,22 @@
-import { ROLE_VIEW } from "./definitions.mjs"
+import { ROLE_ADMIN, ROLE_MEMBER } from "./definitions.mjs"
 
-export function createRoleAccessControl(req, res, next) {
-    if (req.user.role === ROLE_VIEW) {
+export const adminRoleAccessControl = createRoleAccessControlMiddleware(ROLE_ADMIN)
+export const createRoleAccessControl = createRoleAccessControlMiddleware([ROLE_MEMBER, ROLE_ADMIN])
+
+function createRoleAccessControlMiddleware(roles) {
+    return function(req, res, next) {
+        if (!Array.isArray(roles)) {
+            roles = [roles]
+        }
+
+        for (const role of roles) {
+            if (req.user.role === role) {
+                next()
+                return
+            }
+        }
+
         res.error('You don\'t have the correct permisions', 403)
-        return
     }
-
-    next()
 }
 
