e3c5d82e846f0a8fdfb4ce2e3122dee990ac1a95
terrencepeterson <mattbtindall@gmail.com>
2025-07-04 08:54:13 +0100

    Add logged out page

---
+++ frontend/src/views/web/LoggedOutView.vue
@@ -0,0 +1,21 @@
+<script setup>
+import WebAuthRoot from '@/components/web/WebAuthRoot.vue'
+import VButton from '@/components/shared/utilities/VButton.vue'
+</script>
+
+<template>
+    <WebAuthRoot title="You've been logged out" description="Get back into your (work)flow in just a few clicks." width="lg">
+        <div class="flex gap-8 justify-center">
+            <VButton is-link :to="{name: 'index'}" class="px-4">
+                Go to our homepage
+            </VButton>
+            <VButton is-link :to="{name: 'logIn'}" type="primaryOutline">
+                Log back in
+            </VButton>
+        </div>
+    </WebAuthRoot>
+</template>
+
+<style scoped>
+
+</style>
