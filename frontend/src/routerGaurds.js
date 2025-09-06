5c2a4d14600aae90f4fcd93f502be91aeb738226
terrencepeterson <mattbtindall@gmail.com>
2025-09-06 16:23:34 +0100

    Move loggedIn router guard logic to global router gaurd

--- frontend/src/routerGaurds.js
+++ frontend/src/routerGaurds.js
@@ -1,7 +1,4 @@
 import { useStandloneTaskStore } from "@/stores/StandaloneTaskStore"
-import { useUserStore } from "@/stores/UserStore"
-import { useRouter } from "vue-router"
-import { useMessageStore, WARNING_MESSAGE_TYPE } from "./stores/MessageStore"
 
 export function keepDefaultView(to, from) {
     const standAloneTaskStore = useStandloneTaskStore()
@@ -13,17 +10,6 @@ export function keepDefaultView(to, from) {
     to.matched[1].components.appMain = from.matched[1].components.appMain
     if (Object.hasOwn(from.matched[1].components, 'appTopBar')) {
         to.matched[1].components.appTopBar = from.matched[1].components.appTopBar
-    }
-}
-
-export function beforeAuthGaurd() {
-    const userStore = useUserStore()
-    const router = useRouter()
-    const messageStore = useMessageStore()
-
-    if (userStore.isLoggedIn) {
-        messageStore.addMessage(WARNING_MESSAGE_TYPE, `${userStore.name} is already logged in`)
-        router.push({ name: 'dashboard' })
     }
 }
 
